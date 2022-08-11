495271ff2365e3675d4078a78cfd7e44809b3d50
Guilherme <guimagioni@gmail.com>
2022-08-11 18:41:40 -0300

    insert template and list users from db

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem dos Usuários')
+
+@section('content')
+    <h1>Listagem dos usuarios</h1>
+
+    <ul>
+        @foreach($users as $user)
+            <li>
+                {{$user->name}} -
+                {{$user->email}}
+                | <a href="{{route('users.show', $user->id)}}">Detalhes</a>
+
+            </li>
+        @endforeach
+    </ul>
+
+@endsection
